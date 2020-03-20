49d0ff9bb409fcfae5602a91be2b055684dbf105
Jeferson Luis <jgjefersonluis@gmail.com>
2020-03-19 23:46:12 -0300

    Little improvement in board printing

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,9 +5,14 @@ import chess.ChessMatch;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		
+		
+		
+		
 
-		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		//ChessMatch chessMatch = new ChessMatch();
+		//UI.printBoard(chessMatch.getPieces());
 
 		// Board board = new Board(8,8);
 
